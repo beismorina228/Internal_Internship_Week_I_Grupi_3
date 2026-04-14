fa723154870c65be6a8d21215ed502cdc20fda29
dalmashabi <dalmashabi@gmail.com>
2026-04-14 10:51:31 +0200

    Create student_profile.c

---
+++ student_profile.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name: Dalma Shabi\n");
+    printf("School: BGT\n");
+    printf("Group: (vendos numrin e grupit)\n");
+    printf("Week: 1 Day: 1\n");
+
+    return 0;
+}
