723e5dbe852117c0b28c0e6606a2bc76decdc921
Amer9261 <ah211@bgt.school>
2026-04-14 10:40:00 +0200

    Add student profile display in student_profile.c

---
+++ Dita1/student_profile.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Student Name: Amer Hoxha\n");
+    printf("Class: XI-3\n");
+    printf("School: British Gymnasium of Technology\n");
+    printf("Group: Group X\n");
+    return 0;
+}
