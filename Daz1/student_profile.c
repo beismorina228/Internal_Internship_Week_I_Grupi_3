87cc5ad7398f705e6f80169ba1f9dda1aba65857
hotiarbi01-cyber <hotiarbi01@gmail.com>
2026-04-14 10:52:55 +0200

    Create student_profile.c

---
+++ Daz1/student_profile.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("=== Student Profile ===\n");
+    printf("Name: Arbi Hoti\n");
+    printf("City: Prishtina\n");
+    printf("School: British Gymnasium of Technology\n");
+    printf("Field of Interest: Programming & Web Development\n");
+    printf("Skills: C, Java, C#, HTML, CSS\n");
+    printf("Email: your-email@example.com\n");
+
+    return 0;
+}
