3d496eb8ff0101c7fd17bddcbfb150613874201c
Amer9261 <ah211@bgt.school>
2026-04-15 10:31:24 +0200

    Delete Dita1/student_profile.c

--- Dita1/student_profile.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Student Name: Amer Hoxha\n");
-    printf("Class: XI-3\n");
-    printf("School: British Gymnasium of Technology\n");
-    printf("Group: Group X\n");
-    return 0;
-}
