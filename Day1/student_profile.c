0923c853d4bbd5337d8b9bc753fc5b8c8c1159e4
AltinMaliqi <am209@bgt.school>
2026-04-16 09:59:57 +0200

    Add student_profile.c to display student information

---
+++ Day1/student_profile.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Student Profile\n");
+    printf("Name: Altin Maliqi\n");
+    printf("School: British Gymnasium of Technology\n");
+    printf("Group: Grupi 3\n");
+    printf("Week: 1\n");
+    printf("Day: 1\n");
+    return 0;
+}
