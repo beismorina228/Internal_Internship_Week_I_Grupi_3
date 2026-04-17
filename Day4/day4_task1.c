a16e589dc854b354e31c8d3b673721d975b92c8b
Beis Morina <bm228@bgt.school>
2026-04-17 09:38:44 +0200

    Update Day 4 Task 1

---
+++ Day4/day4_task1.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+#define MAX 5
+
+int main() {
+
+    int ids[MAX];
+    char names[MAX][50];
+    int progress[MAX];
+
+    int count = 0;
+    int choice;
+
+    do {
+        printf("\n1. Add Student\n");
+        printf("2. Show Students\n");
+        printf("3. Exit\n");
+        printf("Choice: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+
+            case 1:
+                if (count >= MAX) {
+                    printf("List is full. Cannot add more students.\n");
+                } else {
+                    printf("Enter ID: ");
+                    scanf("%d", &ids[count]);
+
+                    printf("Enter Name: ");
+                    scanf("%s", names[count]);
+
+                    printf("Enter Progress: ");
+                    scanf("%d", &progress[count]);
+
+                    count++;
+                    printf("Student added successfully.\n");
+                }
+                break;
+
+            case 2:
+                if (count == 0) {
+                    printf("No students to show.\n");
+                } else {
+                    for (int i = 0; i < count; i++) {
+                        printf("\nStudent %d\n", i + 1);
+                        printf("ID: %d\n", ids[i]);
+                        printf("Name: %s\n", names[i]);
+                        printf("Progress: %d\n", progress[i]);
+                    }
+                }
+                break;
+
+            case 3:
+                printf("Exiting program...\n");
+                break;
+
+            default:
+                printf("Invalid choice. Try again.\n");
+        }
+
+    } while(choice != 3);
+
+    return 0;
+}
